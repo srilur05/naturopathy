61a8b64e277625ffa34ab1398303e9407483deb1
srikala gajjala <srikala_g@hotmail.com>
2020-12-21 03:37:53 +0100

    new changes made

--- products.js
+++ products.js
@@ -1,16 +1,45 @@
-var checkboxes = document.querySelectorAll('input[type=checkbox]');
-checkboxes.forEach(function(chkbx) {
-  chkbx.onchange = function() {
-    document.getElementById("button").value = getCheckedCheckboxesFor(chkbx.name);
-  }
-})
 
-// Simplified a little this:
-function getCheckedCheckboxesFor(name) {
-  var checkeds = document.querySelectorAll('input[name="' + name + '"]:checked'),
-    values = [];
-  checkeds.forEach(function(chkd) {
-    values.push(chkd.value);
-  });
-  return values;
-}
+
+// for choosing the number of pieces
+
+function numberofpieces(id) {
+  document.getElementById('id').innerHTML = add(); }
+
+  var add = (function () {
+    var counter=0;
+    return function () {counter += 1; return counter;}
+  })();
+  
+
+// total amount for the total number of pieces of the product
+
+function total(b,a) {
+  document.getElementById('b').innerHTML = addition(a); }
+
+  var addition = (function (a) {
+    var counter=0;
+    return function () {counter += a; return counter;}
+  })();
+  
+
+
+
+// grand total for all the products
+
+var grandtotal = document.querySelectorAll('.total');
+
+values = [];
+
+grandtotal.forEach(function(insert) {
+
+    values.push(insert.value); }
+
+  var sum = values.reduce(addup);
+
+  function addup(total, value, index, array) {
+    return total + value; }
+
+  function calculate () {document.getElementById("cart").innerHTML = sum };
+
+
+  
